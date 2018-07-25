e418f00b49e7886c5888d5f452bbf136a48b665c
RAJENDRAN <RajendranAmbika@users.noreply.github.com>
2018-07-25 15:26:59 -0400

    Add files via upload
    collection view variety of samples has been uploaded.

---
+++ SimpleCollectionView/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  SimpleCollectionView
+//
+//  Created by gomathi saminathan on 7/23/18.
+//  Copyright © 2018 Rajendran Eshwaran. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLbl: UILabel!
+    
+}
